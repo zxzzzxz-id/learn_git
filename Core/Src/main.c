d2b497dd85afbc493c8a40ca98b99595c03a66fe
zxzzzxz-id <1730863838@qq.com>
2026-08-03 16:55:46 +0800

    将闪烁次数调整为5

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -51,7 +51,7 @@
 /* USER CODE BEGIN PD */
 /* #define 是宏定义：编译前会把名字替换成后面的内容 */
 #define LED_COUNT   4U    /* 板载 LED 数量，U 表示 unsigned 无符号数 */
-#define BLINK_TIMES 3U    /* 每颗 LED 闪烁次数 */
+#define BLINK_TIMES 5U    /* 每颗 LED 闪烁次数 */
 #define DELAY_MS    250U  /* LED 亮/灭持续时间，单位毫秒 */
 #define BEEP_MS     120U  /* 蜂鸣器响一声的时长 */
 /* USER CODE END PD */
